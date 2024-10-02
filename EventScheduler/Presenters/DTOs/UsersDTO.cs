9e0a01b4221695ea27636a47f26e0d7bb68bad37
Tark_Wight <arsentiy.gonda@gmail.com>
2024-10-03 01:53:44 +0700

    [API] Added event service
    -- Added interface and it's service
    -- Registered service into program

---
+++ EventScheduler/Presenters/DTOs/UsersDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EventScheduler.Presenters.DTOs
+{
+    public class StudentRegistrationForEventDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
