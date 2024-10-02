1e8930df7aa3268f337eabb7baf81772004ae9b9
Tark_Wight <arsentiy.gonda@gmail.com>
2024-10-02 23:59:38 +0700

    [Db] Added Company endpoints
    -- Added company DTO
    -- Added repositoriy

---
+++ EventScheduler/Presenters/DTOs/CompanyDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace EventScheduler.Presenters.DTOs
+{
+    public class CompanyDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CompaniesResponseDTO
+    {
+        public List<CompanyDTO> Companies { get; set; }
+        public PaginationDTO Pagination { get; set; }
+    }
+}
---
+++ EventScheduler/Presenters/DTOs/PaginationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EventScheduler.Presenters.DTOs
+{
+    public class PaginationDTO
+    {
+        public int? Size { get; set; }
+        public int Count { get; set; }
+        public int? Current { get; set; }
+    }
+}
