a1a447c55fe21740959deb723dda605ca9ee1d76
Tark_Wight <arsentiy.gonda@gmail.com>
2024-10-03 07:01:50 +0700

    [fix][API] Added DTO for registration students to event
    -- Added API repository for search user by id
    -- Added DTO { studentId, eventId }

---
+++ EventScheduler/Data/Repositories/APIRepository.cs
@@ -0,0 +1,42 @@
+﻿using EventScheduler.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventScheduler.Data.Repositories
+{
+    public interface IAPIRepository
+    {
+        Task<User?> GetUserByIdAndRoleAsync(Guid userId);
+    }
+    public class APIRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public APIRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            var dean = await _context.Deans.FirstOrDefaultAsync(d => d.Id == userId);
+            if (dean != null)
+            {
+                return dean;
+            }
+
+            var manager = await _context.Managers.FirstOrDefaultAsync(m => m.Id == userId);
+            if (manager != null)
+            {
+                return manager;
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(s => s.Id == userId);
+            if (student != null)
+            {
+                return student;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ EventScheduler/Presenters/DTOs/StudentEventRegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EventScheduler.Presenters.DTOs
+{
+    public class StudentEventRegisterDTO
+    {
+        public Guid studentId { get; set; }
+        public Guid eventId { get; set; }
+    }
+}
